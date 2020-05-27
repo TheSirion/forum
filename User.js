9cf7eef76343728fe896659a32695f4b4b572158
Sirion <thesirion@gmail.com>
2020-05-27 17:31:44 -0300

    fixed auth levels for mods and admins

--- User.js
+++ User.js
@@ -1,5 +1,5 @@
 class User {
-  constructor(username, email, id, picture, auth_level = 0) {
+  constructor(username, email, id, picture, auth_level = 1) {
     this.username = username;
     this.email = email;
     this.id = id;
@@ -10,7 +10,7 @@ class User {
   }
 
   // the AuthLevel defines if the user is a normal forum member, a moderator or
-  // an administrator. 0 = user; 1 = moderator; 2 = administrator. This is why 
+  // an administrator. 1 = user; 2 = moderator; 3 = administrator. This is why 
   // it defaults to 0;
   setAuthLevel(level) {
     if (level > 0 && level < 4) {
@@ -36,14 +36,14 @@ class User {
 
 class Moderator extends User {
   constructor() {
-    super(username, email, id, picture, auth_level = 0);
+    super(username, email, id, picture, auth_level = 2);
 
   }
 }
 
 class Administrator extends Moderator {
   constructor() {
-    super(username, email, id, picture, auth_level = 0);
+    super(username, email, id, picture, auth_level = 3);
 
   }
 }
