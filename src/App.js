76ec98384d4b62557ab1bd31eee4812288a461f1
Matheus <thesirion@gmail.com>
2020-08-22 23:31:27 -0300

    added getters to User object, slightly changed the Header component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React, { Fragment, Drawer } from 'react';
-// import { Button, Avatar, Card, CardContent } from '@material-ui/core';
+import React from "react";
+// import { Drawer, Button } from "@material-ui/core";
+import { BrowserRouter } from "react-router-dom";
 
-import './App.css';
-import './Home.css';
+import "./App.css";
+import "./Home.css";
 
-import SubForumsList from './components/sub-forums-list/sub-forums-list.component';
-import Header from './components/header/header.component';
-import Footer from './components/footer/footer.component';
+import SubForumsList from "./components/sub-forums-list/sub-forums-list.component";
+import Header from "./components/header/header.component";
+import Footer from "./components/footer/footer.component";
 
-function App() {
-  return (
-    <>
-      <Header />
-      <main className='main'>
-        <h2 className='sub-title'>Sub-Forums</h2>
-        <SubForumsList />
-      </main>
-      <Footer />
-    </>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Header />
+        <main className="main">
+          <h2 className="sub-title">Sub-Forums</h2>
+          <SubForumsList />
+        </main>
+        <Footer />
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,15 +1,47 @@
 import React from 'react';
+import { Drawer, ListItem, Button, Divider } from '@material-ui/core'
 
 import './header.styles.scss'
 
-const Header = () => {
-  return (
-    <header className='header'>
-    <div >
-      <h1 className='title'>WEB DEVELOPMENT FORUM</h1>
-    </div>
-  </header>
-  )
+class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      drawerHidden: false,
+    };
+  }
+
+  toggleDrawer = () => {
+    if (this.state.drawerHidden) {
+      this.setState({ drawerHidden: false })
+    } else {
+      this.setState({ drawerHidden: true })
+    }
+  }
+
+  render() {
+    return (
+      <header className='header'>
+        <div>
+          <Button variant='outlined' onClick={this.toggleDrawer}>Options</Button>
+          <h1 className='title'>WEB DEVELOPMENT FORUM</h1>
+          <Drawer
+            variant='temporary'
+            anchor='left'
+            open={this.state.drawerHidden}
+            onClose={this.toggleDrawer}
+          >
+            <ListItem button >Sign In</ListItem>
+            <ListItem button >Sign Up</ListItem>
+            <ListItem button >Settings</ListItem>
+            <ListItem button >Fuck This Shit Up</ListItem>
+            <Divider />
+            <ListItem button onClick={this.toggleDrawer}>Close</ListItem>
+          </Drawer>
+        </div>
+      </header>
+    )
+  }
 }
 
 export default Header;
