2042101fcf99768d43776634da42fbc23c7d627f
Matheus <thesirion@gmail.com>
2020-08-23 00:10:21 -0300

    Updates

---
+++ precache-manifest.e3ef826658cd3471888ffba5e54582de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66cef1c25ea8114736254b6087f76db2",
+    "url": "/forum/index.html"
+  },
+  {
+    "revision": "9722af563ea6af847a66",
+    "url": "/forum/static/css/main.4a026faa.chunk.css"
+  },
+  {
+    "revision": "485a45cd2b3364f95ec6",
+    "url": "/forum/static/js/2.d0527215.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/forum/static/js/2.d0527215.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9722af563ea6af847a66",
+    "url": "/forum/static/js/main.e24917e4.chunk.js"
+  },
+  {
+    "revision": "0359cf8c93fa763e90cf",
+    "url": "/forum/static/js/runtime-main.571cafab.js"
+  }
+]);
