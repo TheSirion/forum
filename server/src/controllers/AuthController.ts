c886c77d1eaea94be0a9ee8b108359fc38b41168
erickLuizA <erickluiz69@gmail.com>
2020-10-13 20:36:25 -0300

    update create register method with validation with yup and jwt logic

--- server/src/controllers/AuthController.ts
+++ server/src/controllers/AuthController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express'
 import bcrypt from 'bcrypt'
+import * as Yup from 'yup'
+import jwt from 'jsonwebtoken'
 
 import database from '../config/connection'
 
@@ -7,13 +9,54 @@ export default class AuthController {
   async create (request: Request, response: Response) {
     const { username, email, password, avatar } = request.body
 
+    const data = {
+      username, email, password, avatar
+    }
+
+    const schema = Yup.object().shape({
+      username: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().min(6).required(),
+      avatar: Yup.string().notRequired()
+    })
+
+    await schema.validate(data, {
+      abortEarly: false
+    })
+
     const hashedPassword = await bcrypt.hash(password, 10)
 
-    const query = 'INSERT INTO users(username, email, password, avatar) VALUES($1, $2, $3, $4) ON CONFLICT DO NOTHING'
+    const query = 'INSERT INTO users(username, email, password, avatar) VALUES($1, $2, $3, $4) RETURNING user_id'
     const values = [username, email, hashedPassword, avatar]
 
-    const { rows } = await database.query(query, values)
+    try {
+      const { rows } = await database.query<{ user_id: number }>(query, values)
 
-    return response.status(201).json(rows)
+      const { user_id } = rows[0]
+
+      const token = jwt.sign({ id: [user_id] }, process.env.LOGIN_TOKEN_SECRET!, {
+        expiresIn: '15min'
+      })
+
+      const refreshToken = jwt.sign({ id: [user_id] }, process.env.REFRESH_TOKEN_SECRET!, {
+        expiresIn: '7d'
+      })
+
+      await database.query('UPDATE users SET refresh_token = $1 WHERE user_id = $2', [refreshToken, user_id])
+
+      response.setHeader('Set-Cookie', [`refreshToken=${refreshToken}; httpOnly;`])
+
+      return response.status(201).json(token)
+    } catch (err) {
+      if (/unique/.test(err)) {
+        const error = new Yup.ValidationError('Unique', err, 'register')
+
+        error.inner = [new Yup.ValidationError('User already exists', err, 'register')]
+
+        throw error
+      } else {
+        throw new Error(err)
+      }
+    }
   }
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,14 @@
 import { config } from 'dotenv'
 import express from 'express'
+import 'express-async-errors'
 
+import errorHandler from './errors/handler'
 import routes from './routes'
 
 config()
 const app = express()
 app.use(express.json())
 app.use(routes)
+app.use(errorHandler)
 
 app.listen(3333, () => console.log('Listening on 3333'))
